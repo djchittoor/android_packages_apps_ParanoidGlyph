52151755c587adc3d2c6b4e5e462da5170e4d098
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2023-02-05 16:14:56 +0100

    [RFC] ParanoidGlyph: Show battery animation on motion detected events
    Change-Id: Ic3885196be2323268d29eb09a1e48a6125e972a9

--- src/co/aospa/glyph/Services/ChargingService.java
+++ src/co/aospa/glyph/Services/ChargingService.java
@@ -23,7 +23,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.BatteryManager;
+import android.os.PowerManager;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -35,66 +40,127 @@ public class ChargingService extends Service {
     private static final boolean DEBUG = true;
 
     private BatteryManager mBatteryManager;
+    private PowerManager mPowerManager;
+    private SensorManager mSensorManager;
+
+    private Sensor mAccelerometerSensor;
+    private static final float ACCELEROMETER_THRESHOLD = 10.0f;
 
     @Override
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
 
         mBatteryManager = (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
+	mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
+	mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+	mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         IntentFilter powerMonitor = new IntentFilter();
         powerMonitor.addAction(Intent.ACTION_POWER_CONNECTED);
         powerMonitor.addAction(Intent.ACTION_POWER_DISCONNECTED);
-        registerReceiver(mPowerMonitor, powerMonitor);
+	registerReceiver(mPowerMonitor, powerMonitor);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (DEBUG) Log.d(TAG, "Starting service");
-        return START_STICKY;
+	    if (DEBUG) Log.d(TAG, "Starting service");
+	    return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
-        if (DEBUG) Log.d(TAG, "Destroying service");
+	    if (DEBUG) Log.d(TAG, "Destroying service");
 
-        onPowerDisconnected();
+	    onPowerDisconnected();
 
-        super.onDestroy();
-        this.unregisterReceiver(mPowerMonitor);
+	    super.onDestroy();
+	    this.unregisterReceiver(mPowerMonitor);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+	    return null;
     }
 
     private int getBatteryLevel() {
-        return mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+	    return mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
     }
 
     private void onPowerConnected() {
-        if (DEBUG) Log.d(TAG, "Power connected");
-        if (DEBUG) Log.d(TAG, "Battery level: " + getBatteryLevel());
-        playChargingAnimation();
+	    if (DEBUG) Log.d(TAG, "Power connected");
+	    if (DEBUG) Log.d(TAG, "Battery level: " + getBatteryLevel());
+	    playChargingAnimation();
+	    registerScreenStateReceiver();
     }
 
+
     private void onPowerDisconnected() {
-        if (DEBUG) Log.d(TAG, "Power disconnected");
+	    if (DEBUG) Log.d(TAG, "Power disconnected");
+	    unregisterReceiver(mScreenStateReceiver);
     }
 
     private void playChargingAnimation() {
-        AnimationManager.playCharging(getBatteryLevel(), this);
+	    AnimationManager.playCharging(getBatteryLevel(), this);
     }
 
     private final BroadcastReceiver mPowerMonitor = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
-                onPowerConnected();
-            } else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
-                onPowerDisconnected();
-            }
-        }
+	    @Override
+	    public void onReceive(Context context, Intent intent) {
+		    if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
+			    onPowerConnected();
+		    } else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
+			    onPowerDisconnected();
+		    }
+	    }
+    };
+
+    private final BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+	    @Override
+	    public void onReceive(Context context, Intent intent) {
+		    if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+			    if (DEBUG) Log.d(TAG, "Screen on");
+			    stopAccelerometerPolling();
+		    } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+			    if (DEBUG) Log.d(TAG, "Screen off");
+			    startAccelerometerPolling();
+		    }
+	    }
+    };
+
+
+    private void startAccelerometerPolling() {
+	    // Register the sensor listener with a rate of SENSOR_DELAY_NORMAL (200ms per event)
+	    mSensorManager.registerListener(mSensorEventListener, mAccelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void stopAccelerometerPolling() {
+	    mSensorManager.unregisterListener(mSensorEventListener);
+    }
+
+    private void registerScreenStateReceiver() {
+	    IntentFilter screenStateFilter = new IntentFilter();
+	    screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
+	    screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+	    registerReceiver(mScreenStateReceiver, screenStateFilter);
+    }
+
+
+    private final SensorEventListener mSensorEventListener = new SensorEventListener() {
+	    @Override
+	    public void onSensorChanged(SensorEvent event) {
+		    float x = event.values[0];
+		    float y = event.values[1];
+		    float z = event.values[2];
+		    float acceleration = (float) Math.sqrt(x * x + y * y + z * z);
+
+		    if (acceleration > ACCELEROMETER_THRESHOLD) {
+			    playChargingAnimation();
+		    }
+	    }
+
+	    @Override
+	    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	    }
     };
 }
